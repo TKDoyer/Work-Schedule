83e93ceef830ecc9981eb78fe3f686622afb0184
Robert Doyer <tkdoyer@gmail.com>
2022-06-05 19:14:30 -0500

    almost done

--- Script.js
+++ Script.js
@@ -1,4 +1,51 @@
-var currentHour = ();
+
+
+// create variable to hold currentHour
+ var currentHour = moment().hour(); // return the current hour. 
+// var currentHour = 10;
+
+var day = (moment().format("[Today is] dddd"))
+$(`#currentDay`).html(day)
+
+
+
+
+var allRows = $('.row') // all rows
+
+function changeColor(rowArray){
+
+    for (let i = 0; i < rowArray.length; i++){
+        
+        var rowHour = rowArray[i].dataset.hour // hour of first row
+        
+        if (rowHour == currentHour){ //comparing
+            //change that row to red
+            $(`.row[data-hour='${rowHour}']`).addClass('present') //adding class to row with data attribute that matches rowHour
+        }
+        if (rowHour < currentHour){
+            // change to gray
+            $(`.row[data-hour='${rowHour}']`).addClass('past')  //adding class to row with data attribute that matches rowHour
+        }
+        if (rowHour > currentHour){
+            // change to green
+            $(`.row[data-hour='${rowHour}']`).addClass('future')  //adding class to row with data attribute that matches rowHour
+        }
+    }
+    
+}
+
+
+
+changeColor(allRows);
+
+
+
+
+
+
+
+
+
 
 
 
--- thingsToDo.md
+++ thingsToDo.md
@@ -6,15 +6,21 @@
     [x] Pull in save icon from openIcons
 
 ## Moment Tasks
- [] get current hour https://momentjs.com/docs/#/get-set/hour/
- [] assign classnames to rows that will allow us to do necessary math
-
-## JQuery Tasks
-
-[] Make function that evaluates current time vs row time
+ [x] get current hour https://momentjs.com/docs/#/get-set/hour/
+    
+ [] Make function that evaluates current time vs row time
+    [x] get a row
+    [x] get time of row
+        [x] add data attribute to each row
+    [] compare that time to current hour
     [] When an hour is in the past, grey it out
     [] When it's the current hour - make the row red
     [] when it's a future hour - make the row green
+ 
+ [] use data attributes in rows to allow for time comparison
+
+## JQuery Tasks
+
 
 
 ## Use JQuery to enable the following
@@ -26,3 +32,8 @@
 
 [] Add current date to top of day planner
 [] Save status
+
+
+
+
+if hour
